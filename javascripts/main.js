f5d1bf1be749bae9fe2f5188ebf01fb73ade4073
redbeyond <redbeyond0718@hotmail.com>
2015-12-05 19:45:50 +0900

    加入了鼠标相应，重新制作了对联图片。

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,17 @@ $(document).ready(function() {
 	$('#midside2Right > * > area').mouseover(function(e) {
 		showMidSide1RightInfo(e);
 	});
+	$('#midside2Left > * > area').mouseover(function(e) {
+		showMidSide1LeftInfo(e);
+	});
 });
 
 function showMidSide1RightInfo(e) {
 	var infoID = e.target.id + 'Nav';
 	$('#midside1Right').html($('#' + infoID).html());
+}
+
+function showMidSide1LeftInfo(e) {
+	var infoID = e.target.id + 'Nav';
+	$('#midside1Left').html($('#' + infoID).html());
 }
